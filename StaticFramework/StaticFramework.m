a739d9645d251b0b4f740d295a0e7f4024a4a789
Cory D. Wiles <kwylez@gmail.com>
2012-01-23 11:59:01 -0500

    Initial Commit

---
+++ StaticFramework/StaticFramework.m
@@ -0,0 +1,23 @@
+//
+//  StaticFramework.m
+//  StaticFramework
+//
+//  Created by Cory D. Wiles on 1/23/12.
+//  Copyright (c) 2012 VW. All rights reserved.
+//
+
+#import "StaticFramework.h"
+
+@implementation StaticFramework
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
