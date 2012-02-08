2e820eb5f7d37a67ccb2f7685938f9533a47780a
Justin DeWind <dewind@atomicobject.com>
2012-02-07 20:22:16 -0500

    ignoring build dir

--- build/StaticFramework.build/Release-iphoneos/StaticFramework-iOS.build/Script-012521DA14CDC9BC00E12214.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-xcodebuild -project ${PROJECT_NAME}.xcodeproj -sdk iphonesimulator -target ${PROJECT_NAME} -configuration ${CONFIGURATION} clean build
-
-xcodebuild -project ${PROJECT_NAME}.xcodeproj -sdk iphoneos -target ${PROJECT_NAME} -configuration ${CONFIGURATION} clean build
-
--- build/StaticFramework.build/Release-iphonesimulator/StaticFramework.build/Objects-normal/i386/StaticFramework.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/cwiles/Desktop/StaticFramework/StaticFramework/StaticFramework.m \
-  /Users/cwiles/Desktop/StaticFramework/StaticFramework/StaticFramework.h
