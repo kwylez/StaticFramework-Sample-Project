4c08af6fe61b86a8d5f857795f6b1becf781f313
Cory D. Wiles <kwylez@gmail.com>
2012-02-10 09:02:52 -0500

    Deleted method signature for helloWorld
    - This class is now the global import

--- StaticFramework/StaticFramework.h
+++ StaticFramework/StaticFramework.h
@@ -1,15 +1,1 @@
-//
-//  StaticFramework.h
-//  StaticFramework
-//
-//  Created by Cory D. Wiles on 1/23/12.
-//  Copyright (c) 2012 VW. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-@interface StaticFramework : NSObject
-
-- (NSString *)helloWorld;
-
-@end
+#import "Sinatra.h"
